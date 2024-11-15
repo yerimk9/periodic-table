8ccd1072f616425b1c53e9cbb17e371b9815a6ee
Kim Yerim <rhrh037@gmail.com>
2024-11-15 17:27:57 +0900

    주기율표 데이터 저장 및 type에 따른 색상 조절

--- common.js
+++ common.js
@@ -1,5 +1,35 @@
+// 인기 리스트 나열
 popularData.forEach((item) => {
   const listItem = document.createElement("li");
   listItem.innerHTML = item;
   $(".js-popularList").append(listItem);
+});
+
+// 주기율표 원소 나열
+
+periodicTable.forEach((el, idx) => {
+  const listItem = `
+  <li class="element-item" data-type="${el.type}" style="background-color: ${
+    colorTypes[el.type]
+  }">
+    <span class="number">${el.number}</span>
+    <p class="symbol">${el.symbol}</p>
+    <p class="name">${el.name}</p>
+    <span class="mass">${el.mass}</span>
+  </li>
+  `;
+
+  $(".periodic-table").append(listItem);
+});
+
+// 원소 유형에 따른 배경 색상 적용
+const element = $(".element-types ul  li");
+element.each((_, el) => {
+  const $el = $(el);
+  const dataType = $el.data("type");
+
+  if (colorTypes[dataType]) {
+    const span = $el.find("span");
+    span.css("background-color", colorTypes[dataType]);
+  }
 });
